cff32fa633aa5a72b8b7de8064b853671a3e326e
Ben Clifford <benc@hawaga.org.uk>
2012-05-03 19:50:00 +0200

    more commentary

--- DNSOps.hs
+++ DNSOps.hs
@@ -26,9 +26,19 @@ module DNSOps where
       x <- a
       return (f x)
 
+-- This is rfc 1034 s5.2.1 option 3 'General Lookup Function'.
+-- It asks "the DNS as a whole" to lookup name,rrtype
+-- At present, it only asks the default resolver, but what I want
+-- eventually is a full DNS lookup with branching and loop-back to happen.
+
   dnslookupDefault name rrtype = maybeListToList <$> (DNSLookup $ withResolver defaultrs $ \resolver -> DNS.lookup resolver name rrtype)
 
--- these two mean to query only a specific DNS server for a value
+-- these two mean to query only a specific DNS server for a value.
+-- whatever they do with the output, they need to perform appropriate
+-- cache and loopback behaviour on the received results.
+-- they are lower level than dnslookupDefault - they are directed at a
+-- specific server and won't go off chasing referals.
+
   dnslookup nsip name rrtype = DNSLookup $ do
     let phn = RCHostName (show nsip)
     res <- makeResolvSeed (ResolvConf phn 3000000 512)
